06bb62d1c621a8692ae3930d46e63ae8d714e4c7
ChatoR3 <ing.rodriguez.rivera@gmail.com>
2023-01-24 16:28:50 -0600

    Se fuciona menú mobile al index, se fuciona css y se agrega evento en el archivo .js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamburgerIcon.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu(){
     console.log('Click');
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS funcionando');
+function toogleMobileMenu(){
+    console.log('Click');
+    mobileMenu.classList.toggle('inactive');
+}
+
