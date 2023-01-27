dc7c9001963db76e53a48ff67601227a05d89c73
ChatoR3 <ing.rodriguez.rivera@gmail.com>
2023-01-26 21:21:35 -0600

    Se fucionacarrito de compras al index, se actualiza el archivo styles.css y se agregan eventos para desplegar el carrito de compras en el archivo main.js

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburgerIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamburgerIcon.addEventListener('click', toogleMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
 
 function toogleDesktopMenu(){
     console.log('Click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+   }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu(){
     console.log('Click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+   }
+
     mobileMenu.classList.toggle('inactive');
 }
 
+function toogleCarritoAside(){
+    console.log('Click');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+         mobileMenu.classList.add('inactive');
+    }
+     
+    aside.classList.toggle('inactive');
+
+}
